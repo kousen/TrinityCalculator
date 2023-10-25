68b883621d4a89212ef8063c0712a76edd1a2fdf
Ken Kousen <ken.kousen@kousenit.com>
2023-10-25 15:25:16 -0400

    Add Company and Employee interfaces and related test
    This commit includes the addition of Company and Employee interfaces with basic method structures. A unit test to test implementations of these interfaces has also been added. This is setup work for upcoming implementation of CompanyEmployee class that will implement these interfaces.

---
+++ src/main/java/edu/trinity/interfaces/Employee.java
@@ -0,0 +1,13 @@
+package edu.trinity.interfaces;
+
+public interface Employee {
+    String getFirst();
+
+    String getLast();
+
+    void doWork();
+
+    default String getName() {
+        return String.format("%s %s", getFirst(), getLast());
+    }
+}
