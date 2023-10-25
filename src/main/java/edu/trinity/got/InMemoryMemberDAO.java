03c14ee68269c2db53b116d2955d1e6fca488144
Ken Kousen <ken.kousen@kousenit.com>
2023-10-25 15:21:06 -0400

    Refactor InMemoryMemberDAO methods returning default values
    InMemoryMemberDAO methods return default or empty values so that students can implement them.

--- src/main/java/edu/trinity/got/InMemoryMemberDAO.java
+++ src/main/java/edu/trinity/got/InMemoryMemberDAO.java
@@ -1,105 +1,77 @@
 package edu.trinity.got;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class InMemoryMemberDAO implements MemberDAO {
     private final Collection<Member> allMembers = MemberDB.getInstance().getAllMembers();
 
     @Override
     public Optional<Member> findById(Long id) {
-        return allMembers.stream()
-                .filter(member -> member.id().equals(id))
-                .findFirst();
+        return Optional.empty();
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return allMembers.stream()
-                .filter(member -> member.name().equals(name))
-                .findFirst();
+        return Optional.empty();
     }
 
     @Override
     public List<Member> findAllByHouse(House house) {
-        return allMembers.stream()
-                .filter(member -> member.house().equals(house))
-                .collect(Collectors.toList());
+        return Collections.emptyList();
     }
 
     @Override
     public Collection<Member> getAll() {
-        return Collections.unmodifiableCollection(allMembers);
+        return Collections.emptyList();
     }
 
     /**
      * Find all members whose name starts with S and sort by id (natural sort)
      */
     public List<Member> startWithSandSortAlphabetically() {
-        return allMembers.stream()
-                .filter(member -> member.name().startsWith("S"))
-                .sorted()
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Final all Lannisters and sort them by name
      */
     public List<Member> lannisters_alphabeticallyByName() {
-        return allMembers.stream()
-                .filter(member -> member.house() == House.LANNISTER)
-                .sorted(Comparator.comparing(Member::name))
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Find all members whose salary is less than the given value and sort by house
      */
     public List<Member> salaryLessThanAndSortByHouse(double max) {
-        return allMembers.stream()
-                .filter(member -> member.salary() < max)
-                .sorted(Comparator.comparing(Member::house))
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Sort members by House, then by name
      */
     public List<Member> sortByHouseNameThenSortByNameDesc() {
-        return allMembers.stream()
-                .sorted(Comparator.comparing(Member::house)
-                        .thenComparing(Comparator.comparing(Member::name).reversed()))
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Sort the members of a given House by birthdate
      */
     public List<Member> houseByDob(House house) {
-        return allMembers.stream()
-                .filter(member -> member.house() == house)
-                .sorted(Comparator.comparing(Member::dob))
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Find all Kings and sort by name in descending order
      */
     public List<Member> kingsByNameDesc() {
-        return allMembers.stream()
-                .filter(member -> member.title() == Title.KING)
-                .sorted(Comparator.comparing(Member::name).reversed())
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Find the average salary of all the members
      */
     public double averageSalary() {
-        return allMembers.stream()
-                .mapToDouble(Member::salary)
-                .average()
-                .orElse(0.0);
+        return 0.0;
     }
 
     /**
@@ -107,55 +79,42 @@ public class InMemoryMemberDAO implements MemberDAO {
      * (note sort by _names_, not members)
      */
     public List<String> namesSorted(House house) {
-        return allMembers.stream()
-                .filter(member -> member.house() == house)
-                .map(Member::name)
-                .sorted()
-                .toList();
+        return Collections.emptyList();
     }
 
     /**
      * Are any of the salaries greater than 100K?
      */
     public boolean salariesGreaterThan(double max) {
-        return allMembers.stream()
-                .mapToDouble(Member::salary)
-                .anyMatch(salary -> salary > max);
+        return false;
     }
 
     /**
      * Are there any members of given house?
      */
     public boolean anyMembers(House house) {
-        return allMembers.stream()
-                .anyMatch(member -> member.house() == house);
+        return false;
     }
 
     /**
      * How many members of a given house are there?
      */
     public long howMany(House house) {
-        return allMembers.stream()
-                .filter(member -> member.house() == house)
-                .count();
+        return 0;
     }
 
     /**
      * Return the names of a given house as a comma-separated string
      */
     public String houseMemberNames(House house) {
-        return allMembers.stream()
-                .filter(member -> member.house() == house)
-                .map(Member::name)
-                .collect(Collectors.joining(", "));
+        return "";
     }
 
     /**
      * Who has the highest salary?
      */
     public Optional<Member> highestSalary() {
-        return allMembers.stream()
-                .max(Comparator.comparing(Member::salary));
+        return Optional.empty();
     }
 
     /**
@@ -163,17 +122,14 @@ public class InMemoryMemberDAO implements MemberDAO {
      * (note: royalty are KINGs and QUEENs only)
      */
     public Map<Boolean, List<Member>> royaltyPartition() {
-        return allMembers.stream()
-                .collect(Collectors.partitioningBy(
-                        member -> member.title() == Title.KING || member.title() == Title.QUEEN));
+        return Collections.emptyMap();
     }
 
     /**
      * Group members into Houses
      */
     public Map<House, List<Member>> membersByHouse() {
-        return allMembers.stream()
-                .collect(Collectors.groupingBy(Member::house));
+        return Collections.emptyMap();
     }
 
     /**
@@ -181,17 +137,14 @@ public class InMemoryMemberDAO implements MemberDAO {
      * (group by house, downstream collector using counting
      */
     public Map<House, Long> numberOfMembersByHouse() {
-        return allMembers.stream()
-                .collect(Collectors.groupingBy(Member::house, Collectors.counting()));
+        return Collections.emptyMap();
     }
 
     /**
      * Get the max, min, and ave salary for each house
      */
     public Map<House, DoubleSummaryStatistics> houseStats() {
-        return allMembers.stream()
-                .collect(Collectors.groupingBy(Member::house,
-                        Collectors.summarizingDouble(Member::salary)));
+        return Collections.emptyMap();
     }
 
 }
