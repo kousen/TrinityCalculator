07bf15546b92fbca93f19cda3d96e08889e61b77
Ken Kousen <ken.kousen@kousenit.com>
2023-10-25 20:00:33 -0400

    Add test for string sorting functionality
    A new test file named SortStrings.java has been added in the edu.trinity.functional package. This file contains two tests, defaultSort() and sortByLength(). The first one tests Java's built-in string sorting and the second one compares strings by length before sorting by natural order. These tests ensure that the expected sorting behaviors are met for arrays of strings.

---
+++ src/test/java/edu/trinity/functional/SortStrings.java
@@ -0,0 +1,32 @@
+package edu.trinity.functional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortStrings {
+    private final String[] strings = "this is a list of strings".split("\\s+");
+
+    @Test
+    void defaultSort() {
+        Arrays.stream(strings)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    @Test
+    void sortByLength() {
+        List<String> sortedStrings = Arrays.stream(strings)
+                .sorted(Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.naturalOrder()))
+                .toList();
+
+        assertThat(sortedStrings).containsExactly(
+                "a", "is", "of", "this", "list", "strings"
+        );
+    }
+}
